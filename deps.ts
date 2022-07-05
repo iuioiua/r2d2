bc6ede26dd05a67c9dbb2f5cafb6d91969acb3e4
Asher Gomez <ashersaupingomez@gmail.com>
2022-07-06 08:33:00 +1000

    update: std modules

--- deps.ts
+++ deps.ts
@@ -1,13 +1,13 @@
-export { writeAll } from "https://deno.land/std@0.146.0/streams/conversion.ts";
-export { BufReader } from "https://deno.land/std@0.146.0/io/buffer.ts";
-export { TextProtoReader } from "https://deno.land/std@0.146.0/textproto/mod.ts";
+export { writeAll } from "https://deno.land/std@0.147.0/streams/conversion.ts";
+export { BufReader } from "https://deno.land/std@0.147.0/io/buffer.ts";
+export { TextProtoReader } from "https://deno.land/std@0.147.0/textproto/mod.ts";
 
 /** Testing */
-export { delay } from "https://deno.land/std@0.146.0/async/delay.ts";
+export { delay } from "https://deno.land/std@0.147.0/async/delay.ts";
 export {
   assertEquals,
   assertRejects,
-} from "https://deno.land/std@0.146.0/testing/asserts.ts";
+} from "https://deno.land/std@0.147.0/testing/asserts.ts";
 
 /** Benchmarks */
 export { connect } from "https://deno.land/x/redis@v0.26.0/redis.ts";

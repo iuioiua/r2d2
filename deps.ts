c532d8acc7a188c949820e8327fe866e7aec9b1e
Asher Gomez <ashersaupingomez@gmail.com>
2022-07-06 17:45:03 +1000

    fixed: formatting

--- deps.ts
+++ deps.ts
@@ -3,7 +3,10 @@ export { BufReader } from "https://deno.land/std@0.147.0/io/buffer.ts";
 export { TextProtoReader } from "https://deno.land/std@0.147.0/textproto/mod.ts";
 
 /** Testing */
-export { assertEquals, assertRejects } from "https://deno.land/std@0.147.0/testing/asserts.ts";
+export {
+  assertEquals,
+  assertRejects,
+} from "https://deno.land/std@0.147.0/testing/asserts.ts";
 
 /** Benchmarks */
 export { connect } from "https://deno.land/x/redis@v0.26.0/redis.ts";

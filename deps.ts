139572fdf32d5896b269f52e4ce0c63fe908c02b
Asher Gomez <ashersaupingomez@gmail.com>
2022-07-06 11:33:37 +1000

    removed: std/async/delay dependency

--- deps.ts
+++ deps.ts
@@ -3,11 +3,7 @@ export { BufReader } from "https://deno.land/std@0.147.0/io/buffer.ts";
 export { TextProtoReader } from "https://deno.land/std@0.147.0/textproto/mod.ts";
 
 /** Testing */
-export { delay } from "https://deno.land/std@0.147.0/async/delay.ts";
-export {
-  assertEquals,
-  assertRejects,
-} from "https://deno.land/std@0.147.0/testing/asserts.ts";
+export { assertEquals, assertRejects } from "https://deno.land/std@0.147.0/testing/asserts.ts";
 
 /** Benchmarks */
 export { connect } from "https://deno.land/x/redis@v0.26.0/redis.ts";

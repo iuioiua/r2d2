eaf848aa9878ac365f84991a843ae5bb181612ed
Asher Gomez <ashersaupingomez@gmail.com>
2022-07-25 21:40:35 +1000

    updated: std module

--- deps.ts
+++ deps.ts
@@ -1,12 +1,12 @@
-export { writeAll } from "https://deno.land/std@0.147.0/streams/conversion.ts";
-export { BufReader } from "https://deno.land/std@0.147.0/io/buffer.ts";
-export { TextProtoReader } from "https://deno.land/std@0.147.0/textproto/mod.ts";
+export { writeAll } from "https://deno.land/std@0.149.0/streams/conversion.ts";
+export { BufReader } from "https://deno.land/std@0.149.0/io/buffer.ts";
+export { TextProtoReader } from "https://deno.land/std@0.149.0/textproto/mod.ts";
 
 /** Testing */
 export {
   assertEquals,
   assertRejects,
-} from "https://deno.land/std@0.147.0/testing/asserts.ts";
+} from "https://deno.land/std@0.149.0/testing/asserts.ts";
 
 /** Benchmarks */
 export { connect } from "https://deno.land/x/redis@v0.26.0/redis.ts";

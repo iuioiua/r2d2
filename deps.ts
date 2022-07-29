862cf9fb0ff8e012860e538e94aed7d92ee49fe6
Asher Gomez <ashersaupingomez@gmail.com>
2022-07-29 16:08:39 +1000

    chore: updated to std@0.150.0

--- deps.ts
+++ deps.ts
@@ -1,11 +1,11 @@
-export { writeAll } from "https://deno.land/std@0.149.0/streams/conversion.ts";
-export { BufReader } from "https://deno.land/std@0.149.0/io/buffer.ts";
+export { writeAll } from "https://deno.land/std@0.150.0/streams/conversion.ts";
+export { BufReader } from "https://deno.land/std@0.150.0/io/buffer.ts";
 
 /** Testing */
 export {
   assertEquals,
   assertRejects,
-} from "https://deno.land/std@0.149.0/testing/asserts.ts";
+} from "https://deno.land/std@0.150.0/testing/asserts.ts";
 
 /** Benchmarks */
 export { connect } from "https://deno.land/x/redis@v0.26.0/redis.ts";

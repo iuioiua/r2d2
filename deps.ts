08bcd8c2eb6af3219fcc5d9d4a85fb6969b1be0c
Asher Gomez <ashersaupingomez@gmail.com>
2022-08-10 09:17:46 +1000

    chore: updated std module

--- deps.ts
+++ deps.ts
@@ -1,12 +1,12 @@
-export { writeAll } from "https://deno.land/std@0.150.0/streams/conversion.ts";
+export { writeAll } from "https://deno.land/std@0.151.0/streams/conversion.ts";
 export {
   BufReader,
   type ReadLineResult,
-} from "https://deno.land/std@0.150.0/io/buffer.ts";
-export { concat } from "https://deno.land/std@0.150.0/bytes/mod.ts";
+} from "https://deno.land/std@0.151.0/io/buffer.ts";
+export { concat } from "https://deno.land/std@0.151.0/bytes/mod.ts";
 
 /** Testing */
 export {
   assertEquals,
   assertRejects,
-} from "https://deno.land/std@0.150.0/testing/asserts.ts";
+} from "https://deno.land/std@0.151.0/testing/asserts.ts";

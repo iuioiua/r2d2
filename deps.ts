e953e88be4b7f8a2b4280ddafeca0a887bb133a4
Asher Gomez <ashersaupingomez@gmail.com>
2022-09-02 13:51:04 +1000

    chore: update std to 0.154.0

--- deps.ts
+++ deps.ts
@@ -1,12 +1,12 @@
-export { writeAll } from "https://deno.land/std@0.152.0/streams/conversion.ts";
+export { writeAll } from "https://deno.land/std@0.154.0/streams/conversion.ts";
 export {
   BufReader,
   type ReadLineResult,
-} from "https://deno.land/std@0.152.0/io/buffer.ts";
-export { concat } from "https://deno.land/std@0.152.0/bytes/mod.ts";
+} from "https://deno.land/std@0.154.0/io/buffer.ts";
+export { concat } from "https://deno.land/std@0.154.0/bytes/mod.ts";
 
 /** Testing */
 export {
   assertEquals,
   assertRejects,
-} from "https://deno.land/std@0.152.0/testing/asserts.ts";
+} from "https://deno.land/std@0.154.0/testing/asserts.ts";

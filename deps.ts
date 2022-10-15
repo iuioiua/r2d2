db64d2d316e49e5622c88ce290e8b63eb9ee0c4f
Asher Gomez <ashersaupingomez@gmail.com>
2022-10-15 22:02:11 +1100

    chore: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,12 +1,12 @@
-export { writeAll } from "https://deno.land/std@0.158.0/streams/conversion.ts";
-export { BufReader } from "https://deno.land/std@0.158.0/io/buffer.ts";
-export { chunk } from "https://deno.land/std@0.158.0/collections/chunk.ts";
+export { writeAll } from "https://deno.land/std@0.159.0/streams/conversion.ts";
+export { BufReader } from "https://deno.land/std@0.159.0/io/buffer.ts";
+export { chunk } from "https://deno.land/std@0.159.0/collections/chunk.ts";
 
 /** Testing */
 export {
   assertEquals,
   assertRejects,
-} from "https://deno.land/std@0.158.0/testing/asserts.ts";
-export { StringReader } from "https://deno.land/std@0.158.0/io/readers.ts";
+} from "https://deno.land/std@0.159.0/testing/asserts.ts";
+export { StringReader } from "https://deno.land/std@0.159.0/io/readers.ts";
 
-export { connect } from "https://deno.land/x/redis@v0.27.0/redis.ts";
+export { connect } from "https://deno.land/x/redis@v0.27.1/redis.ts";
